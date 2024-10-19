dfd06c59aaeae4244ac90b8c9ed6f59341c86045
CenkerAydin <20210808002@ogr.akdeniz.edu.tr>
2024-10-19 18:57:58 +0300

    add Word and WordList model

---
+++ app/src/main/java/com/cenkeraydin/words/data/model/Word.kt
@@ -0,0 +1,12 @@
+package com.cenkeraydin.words.data.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "words")
+data class Word(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val englishWord: String,
+    val turkishWord: String
+)
---
+++ app/src/main/java/com/cenkeraydin/words/data/model/WordList.kt
@@ -0,0 +1,6 @@
+package com.cenkeraydin.words.data.model
+
+data class WordList(
+    val words: List<Word>
+)
+
