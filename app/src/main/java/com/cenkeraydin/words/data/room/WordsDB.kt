9be8f659a4198d70e4a4607c92bab13e0ee35a5d
CenkerAydin <20210808002@ogr.akdeniz.edu.tr>
2024-10-21 15:57:06 +0300

    rename the wordsDao

--- app/src/main/java/com/cenkeraydin/words/data/room/WordsDB.kt
+++ app/src/main/java/com/cenkeraydin/words/data/room/WordsDB.kt
@@ -6,5 +6,5 @@ import com.cenkeraydin.words.data.model.Word
 
 @Database(entities = [Word::class], version = 1, exportSchema = false)
 abstract class WordDatabase : RoomDatabase() {
-    abstract fun wordDao(): WordsDAO
+    abstract fun wordDao(): WordDao
 }
