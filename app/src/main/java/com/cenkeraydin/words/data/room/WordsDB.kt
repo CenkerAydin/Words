ab8695b54e16e796d9c367bfc7c8475a6c74de12
CenkerAydin <20210808002@ogr.akdeniz.edu.tr>
2024-11-16 13:35:10 +0300

    update the version number of database

--- app/src/main/java/com/cenkeraydin/words/data/room/WordsDB.kt
+++ app/src/main/java/com/cenkeraydin/words/data/room/WordsDB.kt
@@ -4,7 +4,7 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.cenkeraydin.words.data.model.Word
 
-@Database(entities = [Word::class], version = 2, exportSchema = false)
+@Database(entities = [Word::class], version = 3, exportSchema = false)
 abstract class WordDatabase : RoomDatabase() {
     abstract fun wordDao(): WordDao
 }
